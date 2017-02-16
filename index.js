52487ee3172b0f4f452416507d072180bdaabff3
Lior Kesos <lior@linnovate.net>
2017-02-17 01:22:19 +0200

    Added babel, graphql and playing with loading schemas from external files

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('foo');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+import {testSchema} from './schema';
+import {graphql} from 'graphql';
+
+var query = '{ hello }';
+graphql(testSchema, query).then(result => {
+
+  // Prints
+  // {
+  //   data: { hello: "world" }
+  // }
+  console.log(result);
+
+});
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: testSchema,
+  graphiql: true
+}));
+console.log('App plistening at 1111');
+app.listen(1111);
