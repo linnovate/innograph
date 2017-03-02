e1ce65961e73c76d4cbef52b3ffab479ce462758
Lior Kesos <lior@linnovate.net>
2017-03-02 18:11:32 +0200

    working towards passing profile

--- schema.js
+++ schema.js
@@ -1,6 +1,8 @@
 // schema.js
-import  Profile from 'innograph-template';
+import template from 'innograph-template';
 import { makeExecutableSchema } from 'graphql-tools';
+import Resolvers from './schemas/social/resolvers';
+import Profile from './schemas/social/profile';
 
 const RootQuery = `
   type RootQuery {
@@ -14,6 +16,11 @@ const SchemaDefinition = `
   }
 `;
 
+console.log('profile:',template.Profile());
+console.log('----');
+console.log('profile2:',Profile());
+
 export default makeExecutableSchema({
-  typeDefs: [SchemaDefinition, RootQuery, Profile]
+  typeDefs: [SchemaDefinition, RootQuery, Profile],
+  resolvers: template.Resolvers
 })
